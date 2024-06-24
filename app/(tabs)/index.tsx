2f88299057f9a81b5073089a1ab370d8652a2d46
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-06-24 09:00:19 -0300

    feat: finished categories button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,9 +11,15 @@ import {
 } from "react-native";
 import { useHeaderHeight } from "@react-navigation/elements";
 import CategoryButtons from "@/components/CategoryButtons";
+import { useState } from "react";
 
 const Page = () => {
   const headerHeight = useHeaderHeight();
+  const [category, setCategory] = useState("All");
+
+  const onCatChanged = (category: string) => {
+    setCategory(category);
+  };
 
   return (
     <>
@@ -67,7 +73,7 @@ const Page = () => {
           </TouchableOpacity>
         </View>
 
-        <CategoryButtons />
+        <CategoryButtons onCayegoryChanged={onCatChanged} />
       </View>
     </>
   );
--- components/CategoryButtons.tsx
+++ components/CategoryButtons.tsx
@@ -5,24 +5,69 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Colors from "@/constants/Colors";
 import destinationCategories from "@/data/categories";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function CategoryButtons() {
+type Props = {
+  onCayegoryChanged: (category: string) => void;
+};
+
+export default function CategoryButtons({ onCayegoryChanged }: Props) {
+  const scrollRef = useRef<ScrollView>(null);
+  const itemRef = useRef<TouchableOpacity[] | null[]>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelectCategory = (index: number) => {
+    const selected = itemRef.current[index];
+    setActiveIndex(index);
+
+    selected?.measure((x) => {
+      scrollRef.current?.scrollTo({ x: x, y: 0, animated: true });
+    });
+
+    onCayegoryChanged(destinationCategories[index].title);
+  };
+
   return (
     <View>
       <Text style={styles.title}>Categories</Text>
-      <ScrollView horizontal>
-        {destinationCategories.map((item) => (
-          <TouchableOpacity onPress={() => {}}>
+      <ScrollView
+        ref={scrollRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          gap: 10,
+          paddingVertical: 10,
+          marginBottom: 10,
+        }}
+      >
+        {destinationCategories.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            ref={(el) => (itemRef.current[index] = el)}
+            onPress={() => handleSelectCategory(index)}
+            style={
+              activeIndex == index
+                ? styles.categoryBtnActive
+                : styles.categoryBtn
+            }
+          >
             <MaterialCommunityIcons
               name={item.iconName as any}
               size={20}
-              color={Colors.black}
+              color={activeIndex == index ? Colors.white : Colors.black}
             />
-            <Text>{item.title}</Text>
+            <Text
+              style={
+                activeIndex == index
+                  ? styles.categoryBtnTxtActive
+                  : styles.categoryBtnTxt
+              }
+            >
+              {item.title}
+            </Text>
           </TouchableOpacity>
         ))}
       </ScrollView>
@@ -35,5 +80,37 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "700",
     color: Colors.black,
+  },
+  categoryBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    borderRadius: 10,
+    shadowColor: "#333333",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+  categoryBtnActive: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.primaryColor,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    borderRadius: 10,
+    shadowColor: "#333333",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+  categoryBtnTxt: {
+    marginLeft: 5,
+    color: Colors.black,
+  },
+  categoryBtnTxtActive: {
+    marginLeft: 5,
+    color: Colors.white,
   },
 });
