f06e1a0b24ce0291a72d345b02127a3df82eedf7
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-07-08 10:58:22 -0300

    feat: added bookmarks icon

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -8,6 +8,8 @@ import {
   TouchableOpacity,
 } from "react-native";
 import React from "react";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 type Props = {
   listings: IPlace[];
@@ -28,12 +30,12 @@ export default function Listings({ listings }: Props) {
   const renderItems: ListRenderItem<IPlace> = ({ item }) => {
     return (
       <TouchableOpacity>
-        <View>
-          <Image
-            source={{ uri: item.image }}
-            style={{ width: 200, height: 200 }}
-          />
-          <Text>{item.name}</Text>
+        <View style={styles.item}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+          <View style={styles.bookmarks}>
+            <Ionicons name="bookmarks" size={20} color={Colors.white} />
+          </View>
+          <Text style={styles.itemTxt}>{item.name}</Text>
         </View>
       </TouchableOpacity>
     );
@@ -51,4 +53,34 @@ export default function Listings({ listings }: Props) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: Colors.white,
+    padding: 10,
+    borderRadius: 10,
+    marginRight: 10,
+    width: 228,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 30,
+  },
+  bookmarks: {
+    backgroundColor: Colors.primaryColor,
+    position: "absolute",
+    top: 185,
+    right: 30,
+    padding: 10,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: Colors.white,
+  },
+  itemTxt: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: Colors.black,
+    marginBottom: 10,
+  },
+});
