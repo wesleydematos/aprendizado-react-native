d9ffc1f4c49d871dd7f9df3ff9b9190c20f3d9a9
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-07-11 10:36:25 -0300

    feat: create group listings and adjust scroll and props at index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,12 +8,15 @@ import {
   View,
   Text,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { useHeaderHeight } from "@react-navigation/elements";
 import CategoryButtons from "@/components/CategoryButtons";
 import { useState } from "react";
 import Listings from "@/components/Listings";
 import listingData from "@/data/destinations.json";
+import GroupListings from "@/components/GroupListings";
+import groupData from "@/data/groups.json";
 
 const Page = () => {
   const headerHeight = useHeaderHeight();
@@ -59,25 +62,30 @@ const Page = () => {
         }}
       />
       <View style={[styles.container, { paddingTop: headerHeight }]}>
-        <Text style={styles.headingTxt}>Explore The Beautiful World!</Text>
-        <View style={styles.searchSectionWrapper}>
-          <View style={styles.searchBar}>
-            <Ionicons
-              name="search"
-              size={18}
-              style={{ marginRight: 5 }}
-              color={Colors.black}
-            />
-            <TextInput placeholder="Search..." placeholderTextColor="#888" />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text style={styles.headingTxt}>Explore The Beautiful World!</Text>
+
+          <View style={styles.searchSectionWrapper}>
+            <View style={styles.searchBar}>
+              <Ionicons
+                name="search"
+                size={18}
+                style={{ marginRight: 5 }}
+                color={Colors.black}
+              />
+              <TextInput placeholder="Search..." placeholderTextColor="#888" />
+            </View>
+            <TouchableOpacity onPress={() => {}} style={styles.filterBtn}>
+              <Ionicons name="options" size={28} color={Colors.white} />
+            </TouchableOpacity>
           </View>
-          <TouchableOpacity onPress={() => {}} style={styles.filterBtn}>
-            <Ionicons name="options" size={28} color={Colors.white} />
-          </TouchableOpacity>
-        </View>
 
-        <CategoryButtons onCayegoryChanged={onCatChanged} />
+          <CategoryButtons onCayegoryChanged={onCatChanged} />
 
-        <Listings listings={listingData} />
+          <Listings listings={listingData} category={category} />
+
+          <GroupListings listings={groupData} />
+        </ScrollView>
       </View>
     </>
   );
